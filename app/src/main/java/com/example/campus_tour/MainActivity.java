792a34f9660b5cf9f47010c7882f26762091b189
karelom <david851215@gmail.com>
2019-12-17 01:17:20 +0800

    adding new feature: Google map change library to androidx

--- app/src/main/java/com/example/campus_tour/MainActivity.java
+++ app/src/main/java/com/example/campus_tour/MainActivity.java
@@ -2,9 +2,14 @@ package com.example.campus_tour;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.Configuration;
-import android.support.v7.app.AppCompatActivity;
+/*support & androidx 會衝突*/
+//import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -22,10 +27,23 @@ public class MainActivity extends AppCompatActivity {
         if (this.getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
             Toast toast = Toast.makeText(context, text1, duration);
             toast.show();
-        } else {
+
+            //建立button，導到Google map 頁面
+            Button map = findViewById(R.id.button);
+            map.setOnClickListener(mapClick);
+        }// 直立式
+        else {
             Toast toast = Toast.makeText(context, text2, duration);
             toast.show();
-        }
+        }// 橫式
 
     }
+
+    private View.OnClickListener mapClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(MainActivity.this, MapsActivity.class);
+            startActivity(intent);
+        }
+    };
 }
